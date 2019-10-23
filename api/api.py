340fc630784e115978683b8bca3a765ff6d88740
xlepotato <yucia181@gmail.com>
2019-10-23 12:09:04 +0800

    prepare deployment

--- api/api.py
+++ api/api.py
@@ -11,9 +11,9 @@ GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--no-sandbox')
-chrome_options.addArguments("--headless");
+chrome_options.add_argument("--headless")
 chrome_options.binary_location = GOOGLE_CHROME_PATH
 
 browser = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
